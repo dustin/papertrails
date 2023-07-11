ecd5e19a8e0ce96295db12fc78abd4d1ef3d5a72
Dustin Sallings <dustin@spy.net>
2023-07-11 11:19:45 -1000

    Use newer default.nix

--- default.nix
+++ default.nix
@@ -1,14 +1,7 @@
 let
-  # Read in the Niv sources
   sources = import ./nix/sources.nix {};
-  # If ./nix/sources.nix file is not found run:
-  #   niv init
-  #   niv add input-output-hk/haskell.nix -n haskellNix
 
-  # Fetch the haskell.nix commit we have pinned with Niv
   haskellNix = import sources.haskellNix {};
-  # If haskellNix is not found run:
-  #   niv add input-output-hk/haskell.nix -n haskellNix
 
   # Import nixpkgs and pass the haskell.nix provided nixpkgsArgs
   pkgs = import
@@ -20,11 +13,15 @@ let
     # the haskell.nix functionality itself as an overlay.
     haskellNix.nixpkgsArgs;
 in pkgs.haskell-nix.project {
+  branchMap = {
+    "https://github.com/brendanhay/amazonka.git" = {
+      "c65c6d0ac9d8fb29cea001713d57aa3aac93f496" = "main";
+    };
+  };
+
   # 'cleanGit' cleans a source directory based on the files known by git
   src = pkgs.haskell-nix.haskellLib.cleanGit {
     name = "haskell-nix-project";
     src = ./.;
   };
-  # Specify the GHC version to use.
-  compiler-nix-name = "ghc902"; # Not required for `stack.yaml` based projects.
 }
